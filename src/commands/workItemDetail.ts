300265eb9e4a2a54540233634cc2ee3961d0d73c
Attilio Capocchiani <attilio.capocchiani@gmail.com>
2021-01-29 10:28:24 +0100

    [WorkItemDetail] Command and template

---
+++ src/commands/workItemDetail.ts
@@ -0,0 +1,44 @@
+import * as moment from "moment";
+import { ViewColumn, window } from "vscode";
+import { TreeItemEntry } from "../dataProviders/treeDataProviders/QueryTreeDataProvider";
+import { WorkItem } from "../interfaces/interfaces";
+
+export default async function openWorkItemDetail(workItem: TreeItemEntry) {
+  console.log(workItem);
+
+  const panel = window.createWebviewPanel(
+    'workItemDetail',
+    workItem.label,
+    ViewColumn.One
+  );
+  panel.webview.html = getWebviewContent((workItem.wrapper as WorkItem).originalObject.fields);
+}
+
+function getWebviewContent(workItem: any): string {
+  return `<html>
+    <body>
+      <h1>${workItem["System.Title"]}</h1>
+      <h3>Assigned To: ${workItem["System.AssignedTo"].displayName}</h3>
+      <h3>${workItem["System.State"]} - ${workItem["System.Reason"]}</h3>
+      <h3>Created: ${moment(workItem["System.CreatedDate"]).format("DD/MM/yyyy HH:mm:ss")} by ${workItem["System.CreatedBy"].displayName}</h3> 
+      <h3>Changed: ${moment(workItem["System.ChangedDate"]).format("DD/MM/yyyy HH:mm:ss")} by ${workItem["System.CreatedBy"].displayName}</h3> 
+      
+      <h2>Description</h2>
+      <p>${workItem["System.Description"] || "&nbsp;"}</p>
+
+      <hr />
+
+      <h2>Custom Fields</h2>
+      ${
+        Object.keys(workItem)
+          .filter((key: string) => key.startsWith("Custom"))
+          .map((key: string) => {
+            const [ _, field ]: string[] = key.split(".");
+            const value = workItem[key];
+            return `<h3>${field}: ${value}</h3>`;
+          })
+          .join("")
+        }
+    </body>
+    </html>`;
+}
